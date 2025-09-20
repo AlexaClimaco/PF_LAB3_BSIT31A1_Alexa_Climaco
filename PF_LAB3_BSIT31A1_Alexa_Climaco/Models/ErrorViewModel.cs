c35e4cfaaff4460ec39e4565a83b2c65408f054f
Alexa Climaco <c1482-23@itmlyceumalabang.onmicrosoft.com>
2025-09-20 14:19:38 +0800

    Initial commit: MVC project running

---
+++ PF_LAB3_BSIT31A1_Alexa_Climaco/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PF_LAB3_BSIT31A1_Alexa_Climaco.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
